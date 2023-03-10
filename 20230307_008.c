aa42541c6abd25e1d3ba7142ca7a9332b4091fc5
Josué Azevedo <josue.a.agomes@gmail.com>
2023-03-09 22:21:08 -0400

    Commit inicial

--- 20230307_008.c
+++ 20230307_008.c
@@ -1,5 +1,15 @@
+#include <stdio.h>
+
 void troca(float *a, float *b) {
     float temp = *a;
     *a = *b;
     *b = temp;
+}
+
+int main() {
+    float x = 10, y = 20;
+    printf("Antes da troca: x = %.2f, y = %.2f\n", x, y);
+    troca(&x, &y);
+    printf("Depois da troca: x = %.2f, y = %.2f\n", x, y);
+    return 0;
 }
